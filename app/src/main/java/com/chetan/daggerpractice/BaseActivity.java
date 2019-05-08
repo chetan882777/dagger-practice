0a34ec1f3b424be6b14f483089b597a1a5d330f6
chetan882777 <chetan882777@gmail.com>
2019-05-08 06:00:55 +0530

    managing authentication with dagger part 3

---
+++ app/src/main/java/com/chetan/daggerpractice/BaseActivity.java
@@ -0,0 +1,73 @@
+package com.chetan.daggerpractice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.chetan.daggerpractice.models.User;
+import com.chetan.daggerpractice.ui.auth.AuthActivity;
+import com.chetan.daggerpractice.ui.auth.AuthResource;
+
+import javax.inject.Inject;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import dagger.android.support.DaggerAppCompatActivity;
+
+import static com.chetan.daggerpractice.ui.auth.AuthResource.AuthStatus.LOADING;
+
+public abstract class BaseActivity extends DaggerAppCompatActivity {
+
+    private static final String TAG = "DaggerExample";
+
+    @Inject
+    public SessionManager sessionManager;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        subscribeObservers();
+    }
+
+    private void subscribeObservers(){
+        sessionManager.getAuthUser().observe(this, new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if(userAuthResource != null){
+                    switch (userAuthResource.status){
+                        case LOADING:{
+                            Log.d(TAG, "onChanged: BaseActivity: LOADING...");
+                            break;
+                        }
+
+                        case AUTHENTICATED:{
+                            Log.d(TAG, "onChanged: BaseActivity: AUTHENTICATED... " +
+                                    "Authenticated as: " + userAuthResource.data.getEmail());
+                            break;
+                        }
+
+                        case ERROR:{
+                            Log.d(TAG, "onChanged: BaseActivity: ERROR...");
+                            break;
+                        }
+
+                        case NOT_AUTHENTICATED:{
+                            Log.d(TAG, "onChanged: BaseActivity: NOT AUTHENTICATED. Navigating to Login screen.");
+                            navLoginScreen();
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void navLoginScreen(){
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
+
