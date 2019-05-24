f5eaa78a31b748ebae95df6d1f98e1dc325de438
chetan882777 <chetan882777@gmail.com>
2019-05-24 09:19:51 +0530

    Recycler view setup

---
+++ app/src/main/java/com/chetan/daggerpractice/util/VerticalSpacingItemDecoration.java
@@ -0,0 +1,21 @@
+package com.chetan.daggerpractice.util;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class VerticalSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int verticalSpaceHeight;
+
+    public VerticalSpacingItemDecoration(int verticalSpaceHeight) {
+        this.verticalSpaceHeight = verticalSpaceHeight;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        outRect.bottom = verticalSpaceHeight;
+    }
+}
